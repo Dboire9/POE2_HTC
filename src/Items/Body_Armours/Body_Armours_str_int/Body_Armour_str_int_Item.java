c089c31a08bca73558e644d453b79157e4359423
doboy9 <dorian.boire98@gmail.com>
2025-10-08 17:01:55 +0200

    Feat : Body_Armour str, dex and int done

---
+++ src/Items/Body_Armours/Body_Armours_str_int/Body_Armour_str_int_Item.java
@@ -0,0 +1,14 @@
+package Items.Body_Armours.Body_Armours_str_int;
+
+import Modifier_class.*;
+import java.util.List;
+
+public class Body_Armour_str_int_Item {
+
+    protected List<Modifier> Normal_allowedPrefixes;
+    protected List<Modifier> Normal_allowedSuffixes;
+    protected List<Modifier> Desecrated_allowedPrefixes;
+    protected List<Modifier> Desecrated_allowedSuffixes;
+    protected List<Modifier> Essences_allowedPrefixes;
+    protected List<Modifier> Essences_allowedSuffixes;
+}

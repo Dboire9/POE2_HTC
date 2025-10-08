1e2c1ef6dc8325a0f9f33bd50de7e1bba065ce0b
doboy9 <dorian.boire98@gmail.com>
2025-10-08 14:25:42 +0200

    Feat : Gloves_dex done

---
+++ src/Items/Gloves/Gloves_dex/Glove_dex_Item.java
@@ -0,0 +1,14 @@
+package Items.Gloves.Gloves_dex;
+
+import Modifier_class.*;
+import java.util.List;
+
+public class Glove_dex_Item {
+
+    protected List<Modifier> Normal_allowedPrefixes;
+    protected List<Modifier> Normal_allowedSuffixes;
+    protected List<Modifier> Desecrated_allowedPrefixes;
+    protected List<Modifier> Desecrated_allowedSuffixes;
+    protected List<Modifier> Essences_allowedPrefixes;
+    protected List<Modifier> Essences_allowedSuffixes;
+}

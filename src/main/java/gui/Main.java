86387f1ce9482ce3ee8aea2ffc7f6cb772c1b3b0
doboy9 <dorian.boire98@gmail.com>
2025-10-09 14:14:29 +0200

    Feat : Made a GUI, possibility to choose a base and the sub base category in the GUI

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,17 +1,32 @@
 package gui;
 
+import core.ItemManager;
+import gui.controllers.CategoryController;
+import gui.controllers.SubCategoryController;
+import gui.views.CategoryView;
+import gui.views.SubCategoryView;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello POE2 Reverse Crafter!");
-        Scene scene = new Scene(label, 400, 200);
-        primaryStage.setTitle("POE2 Crafter");
+        ItemManager manager = new ItemManager();
+
+        CategoryView categoryView = new CategoryView(manager.getCategories());
+        SubCategoryView subCategoryView = new SubCategoryView();
+
+        // Pass the ComboBoxes from the views, plus the manager
+        new CategoryController(categoryView.getComboBox(), subCategoryView.getComboBox(), manager);
+        new SubCategoryController(subCategoryView.getComboBox());
+
+        HBox root = new HBox(20, categoryView, subCategoryView);
+
+        Scene scene = new Scene(root, 800, 400);
+        primaryStage.setTitle("POE2 Reverse Crafter");
         primaryStage.setScene(scene);
         primaryStage.show();
     }

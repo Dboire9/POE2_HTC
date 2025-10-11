232a9b8fef5075b4c8b50c8f3033c9fc9968f1fe
doboy9 <dorian.boire98@gmail.com>
2025-10-11 10:14:37 +0200

    fix: update allowed essence prefixes and suffixes for shields

--- src/main/java/core/Item_modifiers/Off_Hand_Item_modifiers/Shields_Item_modifiers/Shields_Hybrid_Item_modifiers/Modifiers_essences.java
+++ src/main/java/core/Item_modifiers/Off_Hand_Item_modifiers/Shields_Item_modifiers/Shields_Hybrid_Item_modifiers/Modifiers_essences.java
@@ -13,6 +13,9 @@ public class Modifiers_essences {
 	public static final Modifier ESSENCE_INCREASED_PERCENT_ARMOUR;
 	public static final Modifier ESSENCE_INCREASED_PERCENT_EVASION;
 	public static final Modifier ESSENCE_INCREASED_PERCENT_ENERGY_SHIELD;
+	public static final Modifier ESSENCE_HYBRID_INCREASED_PERCENT_ARMOUR_EVASION;
+	public static final Modifier ESSENCE_HYBRID_INCREASED_PERCENT_ARMOUR_ENERGY_SHIELD;
+	public static final Modifier ESSENCE_HYBRID_INCREASED_PERCENT_EVASION_ENERGY_SHIELD;
 	public static final Modifier ESSENCE_INCREASED_SHIELD_BLOCK_PERCENTAGE;
 	public static final Modifier ESSENCE_ABYSS_PREFIX;
 
@@ -75,6 +78,51 @@ public class Modifiers_essences {
 				ModifierSource.ESSENCE,
 				"DefencesPercent",
 				"#% increased Energy Shield");
+
+		ESSENCE_HYBRID_INCREASED_PERCENT_ARMOUR_EVASION = new Modifier(
+			"increased_percent_armour", "increased_percent_evasion",
+			List.of("defences"),
+			List.of(
+					new ModifierTier("Lesser Essence of Enhancement", 16, 1, new Pair<>(27, 42), new Pair<>(27, 42),
+							"increased_percent_armour", "increased_percent_evasion"),
+					new ModifierTier("Essence of Enhancement", 46, 1, new Pair<>(56, 67), new Pair<>(56, 67),
+							"increased_percent_armour", "increased_percent_evasion"),
+					new ModifierTier("Greater Essence of Enhancement", 54, 1, new Pair<>(68, 79), new Pair<>(68, 79),
+							"increased_percent_armour", "increased_percent_evasion")),
+			Modifier.ModifierType.PREFIX,
+			Modifier.ModifierSource.ESSENCE,
+			"DefencesPercent",
+			"#% increased Armour and Evasion");
+
+		ESSENCE_HYBRID_INCREASED_PERCENT_ARMOUR_ENERGY_SHIELD = new Modifier(
+			"increased_percent_armour", "increased_percent_energy_shield",
+			List.of("defences"),
+			List.of(
+					new ModifierTier("Lesser Essence of Enhancement", 16, 1, new Pair<>(27, 42), new Pair<>(27, 42),
+							"increased_percent_armour", "increased_percent_energy_shield"),
+					new ModifierTier("Essence of Enhancement", 46, 1, new Pair<>(56, 67), new Pair<>(56, 67),
+							"increased_percent_armour", "increased_percent_energy_shield"),
+					new ModifierTier("Greater Essence of Enhancement", 54, 1, new Pair<>(68, 79), new Pair<>(68, 79),
+							"increased_percent_armour", "increased_percent_energy_shield")),
+			Modifier.ModifierType.PREFIX,
+			Modifier.ModifierSource.ESSENCE,
+			"DefencesPercent",
+			"#% increased Armour and Energy Shield");
+
+		ESSENCE_HYBRID_INCREASED_PERCENT_EVASION_ENERGY_SHIELD = new Modifier(
+			"increased_percent_evasion", "increased_percent_energy_shield",
+			List.of("defences"),
+			List.of(
+					new ModifierTier("Lesser Essence of Enhancement", 16, 1, new Pair<>(27, 42), new Pair<>(27, 42),
+							"increased_percent_evasion", "increased_percent_energy_shield"),
+					new ModifierTier("Essence of Enhancement", 46, 1, new Pair<>(56, 67), new Pair<>(56, 67),
+							"increased_percent_evasion", "increased_percent_energy_shield"),
+					new ModifierTier("Greater Essence of Enhancement", 54, 1, new Pair<>(68, 79), new Pair<>(68, 79),
+							"increased_percent_evasion", "increased_percent_energy_shield")),
+			Modifier.ModifierType.PREFIX,
+			Modifier.ModifierSource.ESSENCE,
+			"DefencesPercent",
+			"#% increased Evasion and Energy Shield");
 
 		ESSENCE_INCREASED_SHIELD_BLOCK_PERCENTAGE = new Modifier(
 				"increased_shield_block_percentage",

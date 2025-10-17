0a02dacac47d700211fc8245169fb1552a4f801c
doboy9 <dorian.boire98@gmail.com>
2025-10-17 18:15:49 +0200

    Feat : Added the three omens for desecrated modifiers (needs to be implemented)

---
+++ src/main/java/core/Currency/Omens_currency/OmenOfTheLiege.java
@@ -0,0 +1,33 @@
+package core.Currency.Omens_currency;
+
+import core.Crafting.Crafting_Item;
+import core.Crafting.Crafting_Action;
+import core.Currency.Desecrated_currency;
+import core.Currency.Desecrated_currency.guaranteedDesecratedModifier;
+
+public class OmenOfTheLiege extends Omen {
+    public OmenOfTheLiege() {
+        this.name = "Omen of the Liege";
+        this.priority = 1;
+    }
+
+    @Override
+    public Crafting_Item applyEffect(Crafting_Item item, Crafting_Action action) {
+        if (!(action instanceof Desecrated_currency des)) return item;
+
+		if (item.hasOmen(OmenOfTheBlackblooded.class) || item.hasOmen(OmenOfTheSovereign.class)) {
+			System.out.println("Cannot activate Blackblooded Omen while Liege or Sovereign is active!");
+			return item;
+		}
+
+
+        des.guaranteed_des_mod = guaranteedDesecratedModifier.AMANAMU;
+        System.out.println("Omen active: Next Weapon or Jewellery Desecration will guarantee an Amanamu modifier.");
+        return item;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}

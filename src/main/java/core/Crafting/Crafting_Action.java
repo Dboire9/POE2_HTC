69e5b0fa085e0a6c6de1bb6437521e043fcf0271
doboy9 <dorian.boire98@gmail.com>
2025-10-24 16:50:51 +0200

    feat: Reworked the evaluate affix for augmentation orb so we have only candidates with 1 prefix and 1 suffix after augs

--- src/main/java/core/Crafting/Crafting_Action.java
+++ src/main/java/core/Crafting/Crafting_Action.java
@@ -45,7 +45,7 @@ public interface Crafting_Action {
     }
 	
 
-	    default List<Crafting_Candidate> evaluateAffixesFromPreviousOnes(List<Modifier> modifiers, Crafting_Item item, List<Crafting_Candidate> CandidateList, List<Modifier> desiredMods, List<ModifierTier> desiredModTiers, Map<String, Integer> CountDesiredModifierTags) {
+	    default List<Crafting_Candidate> evaluateAffixeswithAug(List<Modifier> modifiers, Crafting_Item item, List<Crafting_Candidate> CandidateList, List<Modifier> desiredMods, List<ModifierTier> desiredModTiers, Map<String, Integer> CountDesiredModifierTags) {
         int score;
 		double stepProbability;
 		boolean isPrefix = false;
@@ -53,10 +53,13 @@ public interface Crafting_Action {
 
 		for (Crafting_Candidate candidate : CandidateList)
 		{
-			item = candidate.copy();
-			List<Crafting_Item> Item_Evaluation = item.addAffixes(modifiers, item);
 			if(modifiers != null && modifiers.get(0).type == ModifierType.PREFIX)
 				isPrefix = true;
+			//For the augmentation orb, we check that we are not applying a modifier on an affix already occupied, so we have only candidates with a prefix and a suffix
+			if((isPrefix && candidate.currentPrefixes[0] != null) || !isPrefix && candidate.currentSuffixes[0] != null)
+				return CandidateListCopy;
+			item = candidate.copy();
+			List<Crafting_Item> Item_Evaluation = item.addAffixes(modifiers, item);
 			for (Crafting_Item items : Item_Evaluation) {
 				score = Crafting_Algorithm.heuristic(items, desiredMods, desiredModTiers, CountDesiredModifierTags);
 				if (score > candidate.score) {

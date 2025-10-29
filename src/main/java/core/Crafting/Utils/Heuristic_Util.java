136751cc0588ef33741188898ac431f3337bd600
doboy9 <dorian.boire98@gmail.com>
2025-10-29 15:46:25 +0100

    feat: enhance crafting logic with improved modifier checks and scoring adjustments, commit before trying not to do omens, but annul everything

--- src/main/java/core/Crafting/Utils/Heuristic_Util.java
+++ src/main/java/core/Crafting/Utils/Heuristic_Util.java
@@ -1,5 +1,6 @@
 package core.Crafting.Utils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,24 +20,31 @@ public class Heuristic_Util {
                 .map(t -> t.text) // Use the text field instead of name
                 .collect(Collectors.toSet());
 
+		List<Modifier> unmatchedMods = new ArrayList<>();
+
 		// Comparing with text, so that the essences can match
         for (Modifier mod : AffixCurrentMods) {
-            if (desiredModifierTexts.contains(mod.text)) {
+            if (desiredModifierTexts.contains(mod.text))
                 matched_modifiers++;
-            }
+			else
+				unmatchedMods.add(mod);
             affix_slots++;
         }
 
 		
-		// If matched modifiers are equal to the slots in the item, add score, so 6000 is the goal to reach(?)
+			// If matched modifiers are equal to the slots in the item, add score, so 6000 is the goal to reach(?)
 			score += 1000 * matched_modifiers;
-			Map<String, Integer> CountModifierTags = CreateCountModifierTags(AffixCurrentMods);
-			score += ScoringTags(CountDesiredModifierTags, CountModifierTags);
+			if(matched_modifiers != affix_slots)
+			{
+				// We do not want to calculate tags of modifiers we know matched
+				Map<String, Integer> CountModifierTags = CreateCountModifierTags(unmatchedMods);
+				score += ScoringTags(CountDesiredModifierTags, CountModifierTags, affix_slots);
+			}
 
 		return score;
 	}
 
-	public static int ScoringTags(Map<String, Integer> CountDesiredModifierTags, Map<String, Integer> CountModifierTags)
+	public static int ScoringTags(Map<String, Integer> CountDesiredModifierTags, Map<String, Integer> CountModifierTags, int affix_slots)
 	{
 		int score = 0;
 
@@ -47,14 +55,14 @@ public class Heuristic_Util {
 			if (CountDesiredModifierTags.containsKey(tag)) {
 				int desiredCount = CountDesiredModifierTags.get(tag);
 		
-				if (currentCount < desiredCount && currentCount > 0) {
+				if (currentCount < desiredCount && currentCount > 0)
 					// If current count is less than desired, but not 0, increase score significantly
 					score += 250 * (desiredCount - currentCount);
-				}
 				else if (currentCount == desiredCount)
-					score += 50;
-				else if ((desiredCount - currentCount) > 1)
-					score -= 500;
+					//The more affix we have the more penalty we want to apply to this
+					score += 50 - (20 * affix_slots);
+				else
+					score -= 900;
 					// If current count is more than desired, decrease score significantly
 					// We tolerate a tag more than what we want because of annul, but no more
 			}

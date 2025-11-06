fb56cff90a6fe4bb752e2cf7a72a63c4babc828a
doboy9 <dorian.boire98@gmail.com>
2025-11-06 23:19:45 +0100

    Feat : Beginning the calculating process

---
+++ src/main/java/core/Crafting/Probability.java
@@ -0,0 +1,26 @@
+package core.Crafting;
+
+
+import java.util.List;
+
+import core.Crafting.Crafting_Candidate;
+import core.Modifier_class.Modifier;
+
+public class Probability {
+	
+
+
+	public static void CalculatingProbability(List<Crafting_Candidate> CompletedPaths, List<Modifier> desiredMod)
+	{
+		for(Crafting_Candidate candidates : CompletedPaths)
+		{
+			// To do : Tier of currency, Omens
+		}
+	}
+
+
+
+
+
+
+}

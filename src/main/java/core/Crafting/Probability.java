a42c87a3e89444ed5a9d3e82f6a0b1d95c7d04c9
doboy9 <dorian.boire98@gmail.com>
2025-11-07 23:24:42 +0100

    Feat : Computing percentage for regals and checking if they can be essences also

--- src/main/java/core/Crafting/Probability.java
+++ src/main/java/core/Crafting/Probability.java
@@ -14,6 +14,7 @@ import core.Currency.ExaltedOrb;
 import core.Currency.RegalOrb;
 import core.Currency.TransmutationOrb;
 import core.Modifier_class.Modifier;
+import core.Modifier_class.ModifierTier;
 import core.Modifier_class.Modifier.ModifierType;
 
 public class Probability {
@@ -55,61 +56,35 @@ public class Probability {
 		ModifierEvent event = candidate.modifierHistory.get(i);
 
 		// Check if it is a desired mod
-		Modifier foundModifier = null;
-		for (Modifier m : desiredMod) {
-			if (m.equals(candidate.modifierHistory.get(i).modifier)) { // or compare by ID, name, family, etc.
-				foundModifier = m;
-				break;
-			}
-		}
-		// It is a desired mods (with the isinstance we retrieve the Crafting_action currency to potentially change it to Greater or Perfect)
-		if(foundModifier != null)
+		Modifier foundModifier = candidate.modifierHistory.get(i).modifier;
+
+		// Checking the level so that we apply the good currency tiers
+		if (foundModifier != null)
 		{
-			// Top tiers are last in list and vice versa so we correct this
 			int realtier = foundModifier.tiers.size() - foundModifier.chosenTier - 1;
-			// We have found the modifier now we need to retrieve the ilvl and compute the percentage of having it with different currency and see if we could have applied an omen
-			if(foundModifier.tiers.get(realtier).level < 35)
-				ComputePercentage(baseItem, candidate, event, 0); // Just apply the normal, we do not change the currency tier
-			else if(foundModifier.tiers.get(realtier).level >= 35 && foundModifier.tiers.get(realtier).level < 50)
-			{// Apply normal and greater
-				ComputePercentage(baseItem, candidate, event, 0);
-				if(event.source instanceof RegalOrb regal)
-					regal.tier = CurrencyTier.BASE;
-				FinalPath.add(candidate);
+			int level = foundModifier.tiers.get(realtier).level;
 
-				// Greater
-				Crafting_Candidate candidate_copy = new Crafting_Candidate();
-				candidate_copy = candidate.copy();
-				candidate_copy.percentage = 0;
-				ComputePercentage(baseItem, candidate_copy, event, 35);
-				if(candidate_copy.modifierHistory.get(i).source instanceof RegalOrb regal)
-					regal.tier = CurrencyTier.GREATER;
-				FinalPath.add(candidate_copy);
+			int[] levels;
+			Crafting_Action.CurrencyTier[] tiers;
+		
+			if (level < 35)
+			{
+				levels = new int[]{0};
+				tiers = new Crafting_Action.CurrencyTier[]{CurrencyTier.BASE};
 			}
-			else if(foundModifier.tiers.get(realtier).level >= 50)
-			{// Apply all (We might change how we do this)
-				ComputePercentage(baseItem, candidate, event, 0);
-				if(event.source instanceof RegalOrb regal)
-					regal.tier = CurrencyTier.BASE;
-				FinalPath.add(candidate);
-
-				// Greater
-				Crafting_Candidate candidate_copy = new Crafting_Candidate();
-				candidate_copy = candidate.copy();
-				candidate_copy.percentage = 0;
-				ComputePercentage(baseItem, candidate_copy, event, 35);
-				if(candidate_copy.modifierHistory.get(i).source instanceof RegalOrb regal)
-					regal.tier = CurrencyTier.GREATER;
-				FinalPath.add(candidate_copy);
-
-				Crafting_Candidate new_candidate_copy = new Crafting_Candidate();
-				new_candidate_copy = candidate.copy();
-				new_candidate_copy.percentage = 0;
-				ComputePercentage(baseItem, new_candidate_copy, event, 35);
-				if(new_candidate_copy.modifierHistory.get(i).source instanceof RegalOrb regal)
-					regal.tier = CurrencyTier.GREATER;
-				FinalPath.add(new_candidate_copy);
+			else if (level < 50)
+			{
+				levels = new int[]{0, 35};
+				tiers = new Crafting_Action.CurrencyTier[]{CurrencyTier.BASE, CurrencyTier.GREATER};
 			}
+			else
+			{
+				levels = new int[]{0, 35, 50};
+				tiers = new Crafting_Action.CurrencyTier[]{CurrencyTier.BASE, CurrencyTier.GREATER, CurrencyTier.PERFECT};
+			}
+		
+			applyTiersAndCompute(baseItem, candidate, event, levels, tiers, FinalPath, i);
+			canBeEssence(baseItem, candidate, event, level, realtier, FinalPath, i);
 		}
 	}
 
@@ -144,12 +119,91 @@ public class Probability {
 		{
 			List<Modifier> PossibleSuffixes = baseItem.base.getNormalAllowedSuffixes();
 		
-			int TotalSuffixWeight = 0;
+			double TotalSuffixWeight = 0;
 			TotalSuffixWeight = baseItem.get_Base_Affixes_Total_Weight_By_Tier(PossibleSuffixes, ilvl);
 			candidate.percentage += event.tier.weight / TotalSuffixWeight;
 		}
 	}
 
+	private static void applyTiersAndCompute(
+		Crafting_Item baseItem,
+		Crafting_Candidate candidate,
+		ModifierEvent event,
+		int[] levels,
+		Crafting_Action.CurrencyTier[] tiers,
+		List<Crafting_Candidate> FinalPath,
+		int i
+	) 
+	{
+		// Computing the percentage for the modifier and then applying the currency tier
+		for (int j = 0; j < levels.length; j++)
+		{
+			Crafting_Candidate c = candidate.copy();
+			c.percentage = 0;
+
+			ComputePercentage(baseItem, c, event, levels[j]);
+
+			if (c.modifierHistory.get(i).source instanceof RegalOrb regal)
+				regal.tier = tiers[j];
+			if (c.modifierHistory.get(i).source instanceof ExaltedOrb exalt)
+				exalt.tier = tiers[j];
+
+			FinalPath.add(c);
+		}
+	}
+
+	private static void canBeEssence(Crafting_Item baseItem, Crafting_Candidate candidate, ModifierEvent event, int level, int realtier, List<Crafting_Candidate> FinalPath, int i)
+	{
+		if(event.modifier.type == ModifierType.PREFIX)
+		{
+			List<Modifier> PossiblePrefixes = baseItem.base.getEssencesAllowedPrefixes();
+
+			// Loop until we find the same family, then check the ilvl to see if we can apply the essence
+			for (Modifier m : PossiblePrefixes)
+			{
+				if (m.family.equals(event.modifier.family))
+				{
+					for(ModifierTier mtiers : m.tiers)
+						if(mtiers.level == level)
+						{
+							Crafting_Candidate c = candidate.copy();
+							// Not doing the augs for now so we hard code
+							c.percentage = 100;
+							c.modifierHistory.get(i).source = new Essence_currency(m.family, mtiers);
+							FinalPath.add(c);
+						}
+
+
+					break;
+				}
+			}
+		}
+
+		if(event.modifier.type == ModifierType.SUFFIX)
+		{
+			List<Modifier> PossibleSuffixes = baseItem.base.getEssencesAllowedSuffixes();
+
+			// Loop until we find the same family, then check the ilvl to see if we can apply the essence
+			for (Modifier m : PossibleSuffixes)
+			{
+				if (m.family.equals(event.modifier.family))
+				{
+					for(ModifierTier mtiers : m.tiers)
+						if(mtiers.level == level)
+						{
+							Crafting_Candidate c = candidate.copy();
+							// Not doing the augs for now so we hard code
+							c.percentage = 100;
+							c.modifierHistory.get(i).source = new Essence_currency(m.family, mtiers);
+							FinalPath.add(c);
+						}
+
+
+					break;
+				}
+			}
+		}
+	}
 
 
 

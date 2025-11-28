dfd5994d7b62e189eda89bdd551fca41ea4be652
doboy9 <dorian.boire98@gmail.com>
2025-11-28 12:20:34 +0100

    Feat : changed files to version 0.9.3

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import CurrencyExclusionPanel from './features/simulation/CurrencyExclusionPanel
 import { useSimulation } from './contexts/SimulationContext';
 
 // Import version from package.json
-const version = '0.9.2';
+const version = '0.9.3';
 
 // Helper function to open external links
 const openExternalLink = (url: string) => {
